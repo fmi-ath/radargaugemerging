30f0e10eaa46df1138896f34de040269a149671e
Seppo Pulkkinen <seppo.pulkkinen@fmi.fi>
2025-04-06 23:34:48 +0300

    Implement regression-Kriging of radar correction factors

--- compute_kriged_correction_factors.py
+++ compute_kriged_correction_factors.py
@@ -11,6 +11,9 @@ The output is two arrays:
   1: gridded gauge-radar correction factors corr = log10(gauge / radar)
   2: Kriging variance of the correction factors
 
+If regression-Kriging is used, the variance is not estimated and its set to
+zero.
+
 Two output formats are supported: GeoTIFF and compressed npz file.
 
 Configuration files (in the config/<profile> directory)
@@ -67,6 +70,7 @@ model = pickle.load(open(args.model, "rb"))
 
 projection = config["grid"]["projection"]
 
+# read configuration and initialize the output grid
 ll_lon = float(config["grid"]["ll_lon"])
 ll_lat = float(config["grid"]["ll_lat"])
 ur_lon = float(config["grid"]["ur_lon"])
@@ -79,27 +83,51 @@ pr = pyproj.Proj(projection)
 ll_x, ll_y = pr(ll_lon, ll_lat)
 ur_x, ur_y = pr(ur_lon, ur_lat)
 
-x = np.linspace(ll_x, ur_x, n_pixels_x + 1)
-x += 0.5 * (x[1] - x[0])
-x = x[:-1]
+grid_x = np.linspace(ll_x, ur_x, n_pixels_x + 1)
+grid_x += 0.5 * (grid_x[1] - grid_x[0])
+grid_x = grid_x[:-1]
 
-y = np.linspace(ll_y, ur_y, n_pixels_y + 1)
-y += 0.5 * (y[1] - y[0])
-y = y[:-1]
+grid_y = np.linspace(ll_y, ur_y, n_pixels_y + 1)
+grid_y += 0.5 * (grid_y[1] - grid_y[0])
+grid_y = grid_y[:-1]
 
 ts = datetime.strptime(args.outtime, "%Y%m%d%H%M")
-z = np.ones((1,)) * ts.timestamp()
+grid_z = np.ones((1,)) * ts.timestamp()
 
 if config["kriging"]["method"] == "ordinary":
-    zvalues, sigmasq = model.execute("grid", x, y, z)
+    zvalues, sigmasq = model.execute("grid", grid_x, grid_y, grid_z)
+
+    zvalues = zvalues[0, :]
+    sigmasq = sigmasq[0, :]
+
+    zvalues.set_fill_value(np.nan)
+    sigmasq.set_fill_value(np.nan)
 else:
-    pass
+    # project radar locations to grid coordinates
+    radar_xy = {}
+    for radar in radar_locs.keys():
+        x, y = pr(radar_locs[radar][0], radar_locs[radar][1])
+        radar_xy[radar] = (x, y)
 
-zvalues = zvalues[0, :]
-sigmasq = sigmasq[0, :]
+    # compute gridded distances to the nearest radar for the regression model
+    dist_grid = util.compute_gridded_distances_to_nearest_radar(
+        ll_x,
+        ll_y,
+        ur_x,
+        ur_y,
+        int(config["grid"]["n_pixels_x"]),
+        int(config["grid"]["n_pixels_y"]),
+        radar_xy,
+    )
 
-zvalues.set_fill_value(np.nan)
-sigmasq.set_fill_value(np.nan)
+    p = dist_grid.flatten()[:, np.newaxis]
+    grid_x, grid_y, grid_z = np.meshgrid(grid_x, grid_y, grid_z)
+    xp = np.column_stack([grid_x.flatten(), grid_y.flatten(), grid_z.flatten()])
+    zvalues = model.predict(p, xp).reshape(grid_x.shape)
+    sigmasq = np.zeros(zvalues.shape)
+
+    zvalues = zvalues[:, :, 0]
+    sigmasq = sigmasq[:, :, 0]
 
 if config["output"]["type"] == "geotiff":
     pr = pyproj.Proj(config["grid"]["projection"])
